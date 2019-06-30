0883445dad7b15a50719eba2800d85d20ef2270d
xuzhong <394704992@qq.com>
2019-06-30 09:51:24 +0800

    首页结构搭建完成

---
+++ src/views/Home.vue
@@ -0,0 +1,151 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-row type="flex" justify="space-between" align="middle">
+        <el-col :span="6">
+          <img src="../assets/img/logo.png" alt>
+        </el-col>
+        <el-col>
+          <h1>品优购后台管理系统</h1>
+        </el-col>
+        <el-col :span="6">
+          <div>
+            前端与移动开发39期
+            <a href="#">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          :default-active="$route.path"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          :router="true"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+
+            <el-menu-item index="/user">
+              <i class="el-icon-menu"></i>
+              <span>用户管理</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+
+            <el-menu-item index>
+              <i class="el-icon-menu"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index>
+              <i class="el-icon-menu"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+
+            <el-menu-item index>
+              <i class="el-icon-menu"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index>
+              <i class="el-icon-menu"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index>
+              <i class="el-icon-menu"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+
+            <el-menu-item index>
+              <i class="el-icon-menu"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+
+            <el-menu-item index="1-4-1">
+              <i class="el-icon-menu"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+
+
+
+<style lang="less">
+.el-container {
+  .el-header {
+    background-color: #b3c1cd;
+    padding: 0;
+    margin: 0;
+    .el-row {
+      .el-col {
+        padding: 0;
+        height: 100%;
+
+        h1 {
+          margin: 0;
+          font-weight: 700;
+          text-align: center;
+        }
+        div {
+          text-align: right;
+          padding: 20px;
+          a {
+            color: orange;
+          }
+        }
+        img {
+          width: 200px;
+        }
+      }
+    }
+  }
+}
+.el-container {
+  height: 100%;
+}
+.el-aside {
+  height: 100%;
+  .el-menu-vertical-demo.el-menu {
+    height: 100%;
+  }
+}
+</style>
+
+
+
+
